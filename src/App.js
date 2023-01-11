674ad718d4708d0abc282fff6f5743cc755c73a9
johnathanbryce <johnathanbryce@rompathlete.com>
2023-01-11 15:28:11 -0800

    added in react dom routing, created and added header & links, other misc changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import Home from './pages/Home/Home';
+
 import './App.css';
 
-/* TESTING GIT */
+// Pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Projects from './pages/Projects/Projects';
+import Skills from './pages/Skills/Skills';
+import Header from './components/Header/Header';
+
+
+// External Packages       <Home />
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
 
 function App() {
-  return (
-    <div>
-      <Home />
 
-    </div>
+
+
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} exact/>
+          <Route path='/contact' element={<Contact/>} exact/>
+          <Route path='/about' element={<About/>} exact/>
+          <Route path='/projects' element={<Projects/>} exact/>
+          <Route path='/skills' element={<Skills/>} exact/>
+        </Routes>
+
+
+    </BrowserRouter>
   );
 }
 
