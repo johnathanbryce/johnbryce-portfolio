1e7da8f18809fbafc338840e81ae8f5f143e003a
johnathanbryce <johnathanbryce@rompathlete.com>
2023-01-12 14:05:21 -0800

    added Contact section and started on skills section. changed page colour themes and adjusted header and various layout components

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,17 +1,82 @@
-import React from 'react';
+import {useState} from 'react';
 import styles from './Contact.module.css';
 
 // Components
 import Header from '../../components/Header/Header';
 import PagesContainer from '../../components/PagesContainer/PagesContainer';
 
+const EMAIL_CONTACT_EXTERNAL = "https://formsubmit.co/johnathanbryce@gmail.com";
+
 function Contact() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleFormSubmit = () => {
+    setTimeout(() => {
+      setFormSubmitted(true);
+    }, 100);
+  };
+
+  // renders when form is submitted
+  if (formSubmitted) {
+    return (
+      <div >
+        <Header />
+        <div className={styles.form_submitted}>
+          <h3> Thank you for reaching out! </h3>
+          <p> I'll respond to your email as soon as possible. </p>
+          <p> - John Bryce</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       
     <Header />
     <PagesContainer>
+      <section className={styles.contact_container}>
+        <div className={styles.text_container}>
+          <h2> Get In Touch </h2>
+          <p> I am currently interested in web development opportunities. If you have other requests or questions, please do not hesitate to reach out! </p>
+          <div className={styles.text_contact_info}>
+            <div>
+              <p> johnathanbryce@gmail.com </p>
+              <p> 604-220-7425 </p>
+            </div>
+            <div>
+              <p><a href="https://www.linkedin.com/in/johnathanbryce/" target="_blank" rel="noreferrer"> linked-in</a></p>
+              <p><a href="https://github.com/johnathanbryce" target="_blank" rel="noreferrer"> github </a> </p>
+            </div>
+          </div>
+        </div>
 
+
+        {/* CONTACT FORM: */}
+        <div className={styles.form_container}>
+          <form
+            action={EMAIL_CONTACT_EXTERNAL}
+            onSubmit={handleFormSubmit}
+            method="POST"
+            target="_blank"
+          >
+            <div className={styles.form_name_email_container}>
+              <input type="text" placeholder="Your name" name="name" required />
+              <input type="email" placeholder="Your email" name="email" required />
+            </div>
+            <input type="text" placeholder="Subject" name="subject"  required/>
+            <textarea
+              placeholder="Your message"
+              name="message"
+              className=""
+              required
+            />
+            <span className={styles.underline}></span>
+            <button className={styles.form_btn} type="submit" >
+              Send Email
+            </button>
+          </form>
+        </div>
+      </section>
       
     </PagesContainer>
     </div>
