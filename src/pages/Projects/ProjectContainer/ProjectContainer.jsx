b88047ec15ac12ea0bbfd87bb9562d66f6161216
johnathanbryce <johnathanbryce@rompathlete.com>
2023-02-01 18:55:41 -0800

    fixed projects layout so less layout clashing, fixed misc responsive sizes, updated romp img

--- src/pages/Projects/ProjectContainer/ProjectContainer.jsx
+++ src/pages/Projects/ProjectContainer/ProjectContainer.jsx
@@ -12,7 +12,7 @@ import {useInView} from 'react-intersection-observer';
 function ProjectContainer({title, description, languages, img, flexDirection, link, github, featuredType}) {
 
   const [ref, inView] = useInView({
-    threshold: 0.95 // 95% of the parent should be in view before initiating animations
+    /* threshold: 0.95 */ // 95% of the parent should be in view before initiating animations
   });
 
 
@@ -26,7 +26,7 @@ function ProjectContainer({title, description, languages, img, flexDirection, li
           x: 0,
           transform: `scale(1)`,
           transition:{
-            type: 'ease-in-out', duration: 0.25,  
+            type: 'ease-in-out', duration: 0.5,  
           }
         })
       }
@@ -35,7 +35,7 @@ function ProjectContainer({title, description, languages, img, flexDirection, li
           opacity: 0.25,
           transform: `scale(0.8)`,
           transition:{
-            type: 'ease-in-out', duration: 0.25,  
+            type: 'ease-in-out', duration: 0.5,  
           },
           x: 0,
          
@@ -51,14 +51,18 @@ function ProjectContainer({title, description, languages, img, flexDirection, li
     <motion.article className={styles.project_container} animate={animateProject}>
       <div className={flexDirection === 'normal' ? styles.project_overlay_wrapper_normal : styles.project_overlay_wrapper_reverse}>
         <div className={flexDirection === 'normal' ? styles.project_container_text : styles.project_container_text_reverse}>
-        <p className={styles.project_overline}> Featured {featuredType} </p>
-        <a href={link} target="_blank" rel='noreferrer'><h3 className={styles.project_title}> {title} </h3> </a>
-        <p className={styles.project_description}> {description}</p>
-        <p className={flexDirection === 'normal' ? styles.project_languages : styles.project_languages_reverse}> {languages}</p>
-        <div className={styles.project_external_links}>
-          <a href={github} target="_blank" rel="noreferrer"><FaGithub/></a>
-          <a href={link} target="_blank" rel="noreferrer"><FaExternalLinkAlt/></a>
-        </div>
+          <div>
+            <p className={flexDirection === 'normal' ? styles.project_overline : styles.project_overline_reverse}> Featured {featuredType} </p>
+            <a href={link} target="_blank" rel='noreferrer'><h3 className={styles.project_title}> {title} </h3> </a>
+          </div>
+            <p className={styles.project_description}> {description}</p>
+          
+            <p className={flexDirection === 'normal' ? styles.project_languages : styles.project_languages_reverse}> {languages}</p>
+            <div className={styles.project_external_links}>
+              <a href={github} target="_blank" rel="noreferrer"><FaGithub/></a>
+              <a href={link} target="_blank" rel="noreferrer"><FaExternalLinkAlt/></a>
+            </div>
+          
       </div>
 
       <a className={flexDirection === 'normal' ? styles.project_container_img : styles.project_container_img_reverse} href={link} target="_blank" rel='noreferrer'>
