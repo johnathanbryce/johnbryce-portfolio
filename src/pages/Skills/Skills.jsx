9d0222fb3006352e371a4f545d98e73a15fd3744
johnathanbryce <johnathanbryce@rompathlete.com>
2023-02-21 12:02:15 -0800

    added icons container w/ responsive design to skills and education section

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -6,6 +6,9 @@ import PagesContainer from '../../components/PagesContainer/PagesContainer';
 import SectionBreakTitle from '../../components/SectionBreakTitle/SectionBreakTitle';
 
 // External Libraries
+import { SiJavascript, SiCss3, SiJquery } from "react-icons/si";
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaReact, FaFigma, FaGithub } from "react-icons/fa";
 import { motion, useAnimation } from "framer-motion"
 import {useInView} from 'react-intersection-observer';
 
@@ -40,54 +43,67 @@ function Skills() {
     <SectionBreakTitle header='skills & education.' />
     <PagesContainer>
       <motion.section ref={ref} animate={animateSectionFadeIn} className={styles.skills_education_container}>
-      
-        <div className={styles.skills_container}>
-          <h3> Skills </h3>
-            <div className={styles.skills_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p> Proficient in HTML, CSS, SASS, JavaScript, jQuery and React </p>
-            </div> 
-              
-            <div className={styles.skills_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p> Design experience using Figma and AdobeXD </p>
-            </div>
+        <div>
+          <div className={styles.skills_container}>
+            <h3> Skills </h3>
+              <div className={styles.skills_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p> Proficient in HTML, CSS, SASS, JavaScript, jQuery and React </p>
+              </div> 
+                
+              <div className={styles.skills_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p> Design experience using Figma and AdobeXD </p>
+              </div>
 
-            <div className={styles.skills_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p> Knowledge and usage of version control using Git and GitHub </p>
-            </div>
+              <div className={styles.skills_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p> Knowledge and usage of version control using Git and GitHub </p>
+              </div>
 
-            <div className={styles.skills_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p> Strong problem-solving and debugging skills </p>
-            </div>
+              <div className={styles.skills_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p> Strong problem-solving and debugging skills </p>
+              </div>
 
-            <div className={styles.skills_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p> Excellent written and verbal communication skills </p>
-            </div>
+              <div className={styles.skills_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p> Excellent written and verbal communication skills </p>
+              </div>
+          </div>
+
+          <div className={styles.education_container}>
+            <h3> Education </h3>
+              <div className={styles.education_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p><a href="https://www.bcit.ca/programs/web-development-fundamentals-statement-of-completion-part-time-0325000000/" target="_blank" rel="noreferrer"> 
+                Web Development Fundamentals, British Columbia Institute of Technology (2021)  </a></p>
+              </div> 
+                
+              <div className={styles.education_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p><a href="https://www.lighthouselabs.ca/en/intro-web-development-course" target="_blank" rel="noreferrer"> 
+                Intro to Web Development, Lighthouse Labs (2020) </a></p>
+              </div>
+
+              <div className={styles.education_flex}>
+                <div className={styles.horizontal_line}> </div>
+                <p><a href="https://www.uvic.ca/socialsciences/psychology/index.php" target="_blank" rel="noreferrer"> 
+                BSc. Psychology, University of Victoria (2017) </a></p>
+              </div>
+          </div>
         </div>
 
-        <div className={styles.education_container}>
-          <h3> Education </h3>
-            <div className={styles.education_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p><a href="https://www.bcit.ca/programs/web-development-fundamentals-statement-of-completion-part-time-0325000000/" target="_blank" rel="noreferrer"> 
-              Web Development Fundamentals, British Columbia Institute of Technology (2021)  </a></p>
-            </div> 
-              
-            <div className={styles.education_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p><a href="https://www.lighthouselabs.ca/en/intro-web-development-course" target="_blank" rel="noreferrer"> 
-              Intro to Web Development, Lighthouse Labs (2020) </a></p>
-            </div>
-
-            <div className={styles.education_flex}>
-              <div className={styles.horizontal_line}> </div>
-              <p><a href="https://www.uvic.ca/socialsciences/psychology/index.php" target="_blank" rel="noreferrer"> 
-              BSc. Psychology, University of Victoria (2017) </a></p>
-            </div>
+        <div className={styles.icons_container}>
+         {/*  <div className={styles.icons_absolute_wrapper}> */}
+            <SiJavascript className={styles.icon_1} />
+            <AiFillHtml5  className={styles.icon_2}/>
+            <SiCss3 className={styles.icon_3}/>
+            <SiJquery className={styles.icon_4}/>
+            <FaReact className={styles.icon_5}/>
+            <FaFigma className={styles.icon_6}/>
+            <FaGithub className={styles.icon_7}/>
+          {/* </div> */}
         </div>
       </motion.section>
       
